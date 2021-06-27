ef5405a8ca260f6d3953143da7d17abf72d3aa48
Troy Williams <troy.williams@bluebill.net>
2021-06-27 17:38:55 -0400

    getting the unit tests working.

--- src/cribbage/cards.py
+++ src/cribbage/cards.py
@@ -12,6 +12,11 @@
 # -----------
 
 """
+A module implement various aspects of the Cribbage objects and scoring system
+
+
+Cribbage Scoring - <https://en.wikipedia.org/wiki/Rules_of_cribbage>
+
 """
 
 # ------------
@@ -26,6 +31,8 @@ from itertools import (
     groupby,
 )
 
+from collections.abc import MutableSequence
+
 # ------------
 # 3rd Party - From pip
 
@@ -36,14 +43,14 @@ from itertools import (
 # -------------
 
 # Shared tuple that stores the card ranks
- = (
+ranks = (
     'A',
     '2', '3', '4', '5', '6', '7', '8', '9',
     'T', 'J', 'Q', 'K',
     )
 
 # shared tuple that stores the card suits
- = ('D', 'H', 'C', 'S')
+suits = ('D', 'H', 'C', 'S')
 
 suit_symbols = {
     'H': u'\u2665',
@@ -70,7 +77,8 @@ rank_sort_order_map = {
 }
 
 
-@dataclass()
+
+@dataclass(frozen=True)
 class Card:
     """
     The Card class represents a playing card with a value rank and suit.
@@ -125,8 +133,10 @@ class Card:
         if self.suit is None:
             raise ValueError('Card.suit is not set!')
 
-        self.rank = self.rank.upper()
-        self.suit = self.suit.upper()
+        # We only want upper case values for the rank and suit and we
+        # have to use this call because the class is frozen
+        object.__setattr__(self, 'rank', self.rank.upper())
+        object.__setattr__(self, 'suit', self.suit.upper())
 
         if self.rank not in ranks:
             raise ValueError(f'INVALID Rank ({self.rank})! Must be one of: {ranks}!')
@@ -180,19 +190,33 @@ class Card:
         using c1 + c2 instead of c1.value() + c2.value()
         """
 
-        return self.value() + other.value()
+        if isinstance(other, Card):
+            return self.value() + other.value()
+
+        return self.value() + other
+
+    def __radd__(self, other):
+        """
+
+        A + B
+
+        This is called if the left object, A doesn't have an __add__
+        defined or it doesn't know how to handle addition to B.
+        """
+
+        return self.value() + other
 
     def __lt__(self, other):
         """
         Make the item sortable
         """
 
-        return rank_sort_order_map[self.value] < rank_sort_order_map[other.value]
+        return rank_sort_order_map[self.rank] < rank_sort_order_map[other.rank]
 
     def __str__(self):
         """
 
-        Return a unicode representation of the card.
+        Return a string representation of the card.
 
         """
 
@@ -211,23 +235,59 @@ def make_deck():
 
     """
 
-    return [Card(*p) p for p in product(ranks, suits)]
+    return [Card(*p) for p in product(ranks, suits)]
 
 
-class Hand(list):
+class Hand(MutableSequence):
     """
     A hand is a simple container for a number of cards.
+
+
+    https://docs.python.org/3/reference/datamodel.html
+    https://docs.python.org/3/library/collections.abc.html
+    https://treyhunner.com/2019/04/why-you-shouldnt-inherit-from-list-and-dict-in-python/
+
     """
 
     def __init__(self, *args):
-        list.__init__(self, *args)
+
+        if len(set(*args)) != len(*args):
+            raise ValueError('Duplicate cards are not allowed!')
+
+        self.values = list(*args)
+
+        # list.__init__(self, *args)
+
+    def __getitem__(self, key):
+        return self.values[key]
+
+    def __setitem__(self, key, value):
+        self.values[key] = value
+
+    def __delitem__(self, key):
+        del self.values[key]
+
+    def __len__(self):
+        return len(self.values)
+
+    def __eq__(self, other):
+        return self.values == other
+
+    def __str__(self):
+        return self.values.__str__()
+
+    def __repr__(self):
+        return self.values.__repr__()
+
+    def insert(self, key, value):
+        self.insert(key, value)
 
     def display(self):
         """
 
         Return a list of strings representing the cards in the hand.
 
-        [AD, 1D, 3S,4C]
+        [AD, 1D, 3S, 4C]
 
         """
 
@@ -235,7 +295,8 @@ class Hand(list):
 
     def cool_display(self):
         """
-        Return a list of strings representing the cards in the hand.
+        Return a list of strings representing the cards in the hand
+        using unicode characters to represent the suits.
 
         [A♦, 1♦, 3♠, 4♣]
         """
@@ -243,28 +304,54 @@ class Hand(list):
 
     def value(self):
         """
-        Return the total value of the hand based on the sum of the
-        individual cards.
+
+        Return the total value of the hand summing each individual card
+        value.
 
         # NOTE
 
-        sum([]) evalutes to 0
+        If the hand is empty, by default the sum function returns 0.
 
         """
+
         return sum([c.value() for c in self])
 
-    def sorted(self):
+    def sort(self, key=None, reverse=False):
         """
-        Return a new ***Hand*** in sorted order.
+        Return a new Hand list in sorted order.
+
+        The new order will account for the rank of the card.
+
+        Given:
+
+        [AD, 4D, 6S, 4C]
+
+        Returns:
+
+        [AD, 4C, 4D, 6S]
+
+        # NOTE
+
+        It may be more preferable to sort from lowest to highest and
+        group-by suit. That is make sure that all diamonds are
+        consecutive.
+
         """
-        # return Hand(sorted(self, key=lambda c: c.sort_order, reverse=False))
-        return Hand(sorted(self))
+
+        self.values.sort(key=key, reverse=reverse)
 
     def every_combination(self, **kwargs):
         """
-        A generator that will yield all possible combination of hands
-        from the current hand. It will iterate through all the single
+
+        Iterate through every combination of cards from 0 to len
+        (self) yielding the result as it is created.
+
+        This is a generator method. It will iterate through all the single
         cards, than pairs than triples etc.
+
+        Optionally, you can specify a `count` and limit the return
+        values to hands of that length. For example if count=2, then
+        all 2 card combinations are returned.
 
         # Parameters (kwargs)
 
@@ -278,16 +365,16 @@ class Hand(list):
         """
 
         if 'count' in kwargs:
-            # From this hand, generate a list of all hands of length count
+            # From this hand, generate a list of all hands of length
+            # count
 
-            # If count = 2
-            # hand = [x,y,z,w]
+            # If count = 2 and m = [x,y,z,w]
 
             # >>>from itertools import combinations
             # >>> m = ['x', 'y', 'z', 'w']
             # >>> [c for c in combinations(m,2)]
-            # [('x', 'y'), ('x', 'z'), ('x', 'w'), ('y', 'z'), ('y', 'w'), ('z', 'w')]
-
+            # [('x', 'y'), ('x', 'z'), ('x', 'w'),
+            #  ('y', 'z'), ('y', 'w'), ('z', 'w')]
 
             for combo in combinations(self, kwargs['count']):
                 yield Hand(combo)
@@ -303,11 +390,18 @@ class Hand(list):
             # >>> m = ['x', 'y', 'z', 'w']
             # >>> [r for r in range(len(m) + 1)]
             # [0, 1, 2, 3, 4]
-            # >>> [c for c in chain.from_iterable(combinations(m, r) for r in range(len(m) + 1))]
-            # [(), ('x',), ('y',), ('z',), ('w',), ('x', 'y'), ('x', 'z'), ('x', 'w'), ('y', 'z'), ('y', 'w'), ('z', 'w'), ('x', 'y', 'z'), ('x', 'y', 'w'), ('x', 'z', 'w'), ('y', 'z', 'w'), ('x', 'y', 'z', 'w')]
+            # >>> [c for c in chain.from_iterable(
+            #   combinations(m, r) for r in range(len(m) + 1))]
+            # [(), ('x',), ('y',), ('z',), ('w',), ('x', 'y'),
+            #  ('x', 'z'), ('x', 'w'), ('y', 'z'), ('y', 'w'), ('z', 'w'),
+            #  ('x', 'y', 'z'), ('x', 'y', 'w'), ('x', 'z', 'w'),
+            #  ('y', 'z', 'w'), ('x', 'y', 'z', 'w')]
 
-            for combo in chain.from_iterable(combinations(self, r) for r in range(len(self) + 1)):
+            for combo in chain.from_iterable(
+                combinations(self, r) for r in range(len(self) + 1)
+            ):
                 yield Hand(combo)
+
 
 def deal_hand(deck, count):
     """
@@ -331,11 +425,326 @@ def deal_hand(deck, count):
     and then 5 cards to the dealer. They are dealt alternating
     starting with the pones hand, then the dealer, back and forth.
 
+    May need to implment fisher-yates algorithm
+        - https://emctackett.medium.com/fisher-yates-shuffle-randomly-shuffle-a-list-in-place-30a05b05a9cb
+
     """
 
     hand = Hand(deck[:count])
     del deck[:count]
 
     return hand
+
+
+def find_fifteens(hand):
+    """
+
+    Given a hand, find all of the combination of cards that sum to 15.
+
+    This is a generator method that will iterate through all of the card
+    combinations in the hand for combinations that sum to 15. When a
+    combination is found that sums to 15 it is returned and the
+    iteration is paused.
+
+    # Parameters
+
+    hand:Hand
+        - The list of cards to find combinations that adds to 15.
+
+    # Return
+
+    For each iteration of this method, a Hand containing the combination
+    that adds to 15.
+
+    """
+    for combo in hand.every_combination():
+        if combo.value() == 15:
+            yield combo
+
+def find_pairs(hand):
+    """
+    Given a hand, fine all of the combination of cards that are pairs,
+    that is, cards that contain matching ranks.
+
+    A generator that will iterate through all of the combinations and
+    yield pairs of cards.
+    """
+
+    for left, right in hand.every_combination(count=2):
+        if left.rank == right.rank:
+            yield combo
+
+    # for combo in hand.every_combination(count=2):
+    #     if combo[0].rank == combo[1].rank:
+    #         yield combo
+
+def find_runs(hand):
+    """
+    Determine all runs within the Hand.
+
+    A run is at least 3 Cards containing a sequence of incrementing rank
+    values. For example:
+
+    [1,2,3]
+
+    [4,5,6]
+
+    [T,J,Q,K]
+
+    This method is a generator that that will find all runs of 3 or more
+    Cards yielding the combinations.
+
+    """
+    runs = []
+    for combo in chain.from_iterable(combinations(hand, r)
+                                     for r in range(3, len(hand)+1)):
+
+        for k, g in groupby(enumerate(Hand(sorted(combo))),
+                            lambda ix: ix[0] - ix[1].sort_order):
+
+            # strip out the enumeration and get the cards in the group
+            new_hand = Hand([i[1] for i in g])
+            if len(new_hand) < 3:
+                continue
+
+            m = set(new_hand)
+
+            # check to see if the new run is a subset of an existing run
+            if any([m.issubset(s) for s in runs]):
+                continue
+
+            # if the new run is a super set of previous runs, we need to remove
+            # them
+            l = [m.issuperset(s) for s in runs]
+            if any(l):
+                runs = [r for r, t in zip(runs, l) if not t]
+
+            if m not in runs:
+                runs.append(m)
+
+    return [Hand(list(r)).sorted() for r in runs]
+
+
+def find_flushes(hand, cut):
+    """
+    Find the flushes, only 4 card or 5 card flushes allowed
+
+    Return the hand containing the number of cards
+
+    """
+
+    flush_4 = set([c.suit for c in hand])
+    flush_5 = flush_4.add(cut.suit)
+
+    if len(flush_5) == 1:
+        return hand + [cut]
+
+    if len(flush_4) == 1:
+        return hand
+
+    return []
+
+
+def find_combinations(hand, cut):
+    """
+    Given the pone hand and cut card, find all interesting combinations
+    of cards. We are looking for:
+
+    - 15 - Search for all card combinations (hand and cut card
+      combined), with the ranks adding to 15. E.g.: [5, 10], [2,3,10]
+
+    - pairs - Search for all card pairs with matching ranks. That is two
+      cards of the same rank e.g. [2D, 2S]
+
+        - NOTE: Triples (2,2,2) and 4 of a kind (2,2,2,2) are simple
+          scored by the number of combinations of pairs. A triple has 3
+          pairs and would score 6 and a quad would have 6 pairs scoring
+          12. This method would list the pairs and score correctly
+
+    - straight/runs - A sequence of 3 or more cards, by rank e.g.
+      [4H, 5D, 6S]. Smaller runs from a larger run are not counted. If
+      you have [1,2,3,4], you cannot count [1,2,3] or [2,3,4]. If you
+      have [2,3,3,4] you have a double run of [2,3,4]
+
+    - flush - At least 4 cards of the same suit.
+
+    - nobs - jack of same suit as starter/cut card
+
+    - nibs - the cut card is a jack
+
+    # Parameters
+
+    hand:Hand
+        - The hand we want to score.
+
+    cut:Card
+        - The cut card that will be counted along with the hand.
+
+    # Return
+
+    A dictionary containing all card combinations of interest with the
+    following keys:
+    - 'fifteen' - A list of cards that sum to 15
+    - 'pair' - All of the car pair combinations found in the hand
+    - 'run' - All of the runs found in the hand
+    - 'flush' - All of the flush cards found in the hand
+    - 'nobs' - The nobs card found in the hand.
+    - 'nibs' - The cut card is a Jack.
+
+    """
+
+    full_hand = Hand(hand + [cut])
+
+    return {
+        'fifteen':list(find_fifteens(full_hand)),
+        'pair':list(find_pairs(full_hand)),
+        'run':list(find_runs(full_hand)),
+        'flush':find_flushes(hand, cut),
+        'nobs': [c for c in hand if c.suit == cut.suit and c.rank == 'J'],
+        'nibs': [cut] if cut.rank == 'J' else [],
+    }
+
+
+def score(hands):
+    """
+    Once the combinations are discovered within the hand, this method
+    will take the dictionary containing the combinations and score
+    them. The dictionary is keyed as follows:
+
+    - 'fifteen' - A list of cards that sum to 15
+    - 'pair' - All of the car pair combinations found in the hand
+    - 'run' - All of the runs found in the hand
+    - 'flush' - All of the flush cards found in the hand
+    - 'nobs' - The nobs card found in the hand.
+    - 'nibs' - The jack cut card.
+
+    This method will score the items as follows:
+
+    - 'fifteen' - 2 points for every combination
+    - 'pair' - 2 points for every pair
+    - 'run' - 1 point per card in each of the runs
+    - 'flush' - 1 point per card in the flush
+    - 'nobs' - 1 point for nobs
+    - 'nibs' - 2 points for his heels
+
+    # Parameters
+
+    hands:dict
+        - The dictionary containing the hand combinations to score
+
+    # Return
+
+    A dictionary containing scores for the card combinations of interest
+    with the following keys:
+    - 'fifteen' - 2 points per card combination that sums to 15
+    - 'pair' - 2 points per pair (triples and quads are accounted for)
+    - 'run' - 1 point per card in each run
+    - 'flush' - 1 point per card in the flush
+    - 'nobs' - 1 point for nob
+    - 'nibs' - 2 points for his heels
+
+    """
+
+    return {
+        'fifteen': len(hands['fifteen'])*2,
+        'pair': len(hands['pair'])*2,
+        'run': sum([len(r) for r in hands['run']]),
+        'flush': len(hands['flush']),
+        'nobs': len(hands['nobs']),
+        'nibs': len(hands['nibs'])*2,
+    }
+
+
+def score_hand(hand, cut, **kwargs):
+    """
+    Score the pones hand and return a list of strings that can be
+    printed.
+
+    # Parameters
+    hand:Hand
+        - The hand to score
+
+    cut:Card
+        - The cut card to score with the hand
+
+    # Parameters (kwargs)
+
+    include_nibs:bool
+        - Include the calculation of nibs.
+        - Only dealer can claim this.
+        - Default - False
+
+    five_card_flush:bool
+        - Only count the flush if it is 5 cards.
+        - Only applied when counting the crib.
+        - Default - False
+
+    """
+
+    include_nibs = kwargs.get('include_nibs', False)
+    five_card_flush = kwargs.get('five_card_flush', False)
+
+    hands = find_combinations(hand, cut)
+    hand_scores = score(hands)
+
+    # Flush is worth 5 points and only 5 points if we are counting the
+    # crib (4 crib cards + cut). If we are not counting the crib we can
+    # use the value of hand_scores directly.
+    flush = 5 if hand_scores["flush"] == 5 and five_card_flush else hand_scores["flush"]
+
+    # calculate the total excluding the nibs and flush
+    total = sum([v for k, v in hand_scores.items() if k not in ["nibs", "flush"]])
+
+    # add the flush separately based on the flush calculation above.
+    total += flush
+
+    # Are we counting the dealer hand?
+    if include_nibs:
+        total += hand_scores["nibs"]
+
+    summary = [
+        f'Hand      = {hand.sorted().cool_display()}',
+        f'Cut       = {cut.cool_display()}',
+        f'{len(hands["fifteen"])} Fifteens for {hand_scores["fifteen"]}',
+        f'{len(hands["pair"])} Pairs for    {hand_scores["pair"]}',
+        f'{len(hands["run"])} Runs for     {hand_scores["run"]}',
+        f'Flush for      {flush}',
+        f'Nobs for       {hand_scores["nobs"]}',
+        f'Nibs for       {hand_scores["nibs"] if include_nibs else 0}',
+        '-----------------',
+        f'Total          {total}',
+    ]
+
+    if hands['fifteen']:
+
+        items = ['', 'Fifteens ====']
+        for f in hands['fifteen']:
+            items.append(f'{f.cool_display()} = 15')
+
+        summary.extend(items)
+
+    if hands['pair']:
+
+        items = ['', 'Pairs ====']
+        for f in hands['pair']:
+            items.append(f'{f.cool_display()}')
+
+        summary.extend(items)
+
+    if hands['run']:
+
+        items = ['', 'Runs ====']
+
+        for f in hands['run']:
+            items.append(f'{f.cool_display()}')
+
+        summary.extend(items)
+
+    return summary
+
+
+
+
+
 
 
---
+++ tests/test_cribbage.py
@@ -0,0 +1,314 @@
+#!/usr/bin/env python3
+#-*- coding:utf-8 -*-
+
+# -----------
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2021 Troy Williams
+
+# uuid:   6323e100-d778-11eb-822d-e7a609cf0d07
+# author: Troy Williams
+# email:  troy.williams@bluebill.net
+# date:   2021-06-27
+# -----------
+
+"""
+Test module for testing them the cribbage methods.
+"""
+
+# ------------
+# System Modules - Included with Python
+
+
+# ------------
+# 3rd Party - From pip
+
+import pytest
+
+# ------------
+# Custom Modules
+
+from cribbage.cards import (
+    Card,
+    make_deck,
+    Hand,
+)
+
+# -------------
+
+
+
+# -------------
+# Test Card - init
+
+# suits = 'D', 'H', 'C', 'S'
+# rank  = 'A', '2','3','4','5','6','7','8','9','T','J','Q','K'
+
+data = [
+    ('8D', Card(rank='8', suit='D')),
+    ('8d', Card(rank='8', suit='D')),
+    ('9H', Card(rank='9', suit='H')),
+    ('9H', Card('9', 'H')),
+    ('9H', Card(*'9H')),
+    ('TC', Card('T', 'C')),
+    (('2','H'), Card(rank='2', suit='H')),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_init(data):
+
+    left, right = data
+
+    c = Card(*left)
+
+    assert c == right
+
+
+# -------------
+# Test Card - value
+
+data = [
+    ('8D', 8),
+    ('9H', 9),
+    ('TH', 10),
+    ('AS', 1),
+    ('KC', 10),
+    ('JD', 10),
+    ('QS', 10),
+    ('2C', 2),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_value(data):
+
+    left, right = data
+
+    c = Card(*left)
+
+    assert c.value() == right
+
+# -------------
+# Test Card - str
+
+data = [
+    ('8D', '8D'),
+    ('9H', '9H'),
+    ('TH', 'TH'),
+    ('AS', 'AS'),
+    ('KC', 'KC'),
+    ('JD', 'JD'),
+    ('QS', 'QS'),
+    ('2C', '2C'),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_str(data):
+
+    left, right = data
+
+    c = Card(*left)
+
+    assert str(c) == right
+
+
+# -------------
+# Test Card - cool display
+
+data = [
+    ('8D', '8' + u'\u2666'),
+    ('9H', '9' + u'\u2665'),
+    ('TH', 'T' + u'\u2665'),
+    ('AS', 'A' + u'\u2660'),
+    ('KC', 'K' + u'\u2663'),
+    ('JD', 'J' + u'\u2666'),
+    ('QS', 'Q' + u'\u2660'),
+    ('2C', '2' + u'\u2663'),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_cool(data):
+
+    left, right = data
+
+    c = Card(*left)
+
+    assert c.cool_display() == right
+
+
+# -------------
+# Test Card - add
+
+data = [
+    (('8D', '9C'), 17),
+    (('AS', '9C', '3H'), 13),
+    (('6C', '9C'), 15),
+    (('TD', 'JC', 'AS'), 21),
+    (('TD', 'JC', 'KS'), 30),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_add(data):
+
+    left, right = data
+
+    value = sum([Card(*c) for c in left])
+
+    assert value == right
+
+# -------------
+# Test Card - sort
+
+
+data = [
+    (('TC', 'AS', '3H'), ('AS', '3H', 'TC')),
+    (('TC', 'AS', '3H', '6C', '4D'), ('AS', '3H', '4D', '6C', 'TC')),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_sort(data):
+
+    left, right = data
+
+    cards = sorted([Card(*c) for c in left])
+
+    assert tuple(str(c) for c in cards) == right
+
+# -------------
+# Test Card - Exceptions
+
+data = [
+    'H5',
+    'S6',
+    'SS',
+    'A ',
+    '1S',
+    'GH',
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_exception(data):
+
+    with pytest.raises(ValueError):
+        c = Card(*data)
+
+
+# -------------
+# Test make_deck
+
+def test_card_sort():
+
+    deck = make_deck()
+
+    assert len(deck) == 52
+
+    assert Card(*'3H') in deck
+
+
+# -------------
+# Test Hand - Init
+
+
+data = [
+    (('TC', 'AS', '3H'), Hand([Card(*'TC'), Card(*'AS'), Card(*'3H')])),
+    (('9C', 'AS', '3D'), Hand([Card(*'9C'), Card(*'AS'), Card(*'3D')])),
+    (('9C', 'AS', '3D', 'Ac', 'ah'), Hand([Card(*'9C'), Card(*'AS'), Card(*'3D'), Card(*'AC'), Card(*'AH')])),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_hand_init(data):
+
+    left, right = data
+
+    cards = Hand([Card(*c) for c in left])
+
+    assert cards == right
+
+# -------------
+# Test Hand - Init - Exception - Duplicates
+
+data = [
+    ('TC', 'AS', '3H', 'tc'),
+    ('9C', 'AS', 'as', '3D'),
+    ('9C', 'AS', '3D',  '3D', 'Ac', 'ah'),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_card_exception(data):
+
+    with pytest.raises(ValueError):
+        cards = Hand([Card(*c) for c in data])
+
+# -------------
+# Test Hand - Display
+
+data = [
+    (('TC', 'AS', '3H'), ('TC', 'AS', '3H') ),
+    (('9C', 'AS', '3D'), ('9C', 'AS', '3D') ),
+    (('9C', 'AS', '3D', 'Ac', 'ah'), ('9C', 'AS', '3D', 'AC', 'AH') ),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_hand_display(data):
+
+    left, right = data
+
+    cards = Hand([Card(*c) for c in left])
+
+
+    items = cards.display()
+
+    # writing the test like this because we don't care (at this point)
+    # if it is a list or tuple, just that the elements are the same and
+    # in the same order.
+
+    assert len(items) == len(right)
+    assert all([a == b for a, b in zip(items, right)])
+
+
+
+# -------------
+# Test Hand - Value
+
+data = [
+    (('TC', 'AS', '3H'),  14),
+    (('9C', 'AS', '3D'),  13),
+    (('9C', 'AS', '3D', 'Ac', 'ah'),  15),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_hand_value(data):
+
+    left, right = data
+
+    assert Hand([Card(*c) for c in left]).value() == right
+
+
+# -------------
+# Test Hand - Sorted
+
+data = [
+    (('TC', 'AS', '3H'), ('AS', '3H', 'TC') ),
+    (('9C', 'AS', '3D'), ('AS', '3D', '9C') ),
+    (('9C', 'AS', '3D', 'Ac', 'ah'), ('AS', 'AC', 'AH', '3D', '9C')),
+]
+
+@pytest.mark.parametrize('data', data)
+def test_hand_sorted(data):
+
+    left, right = data
+
+    cards = Hand([Card(*c) for c in left])
+
+    assert isinstance(cards, Hand)
+
+    # Create a new sorted list
+    items = Hand(sorted(cards)).display()
+
+    assert len(items) == len(right)
+    assert all([a == b for a, b in zip(items, right)])
+
+    # Sort the Cards inline
+    cards.sort()
+    items = cards.display()
+
+    assert len(items) == len(right)
+    assert all([a == b for a, b in zip(items, right)])
